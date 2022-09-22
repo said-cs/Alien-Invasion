8fb2bdfa4d2ca64b2cf1c1770db867af7ee6d47d
said <charef.said@gmail.com>
2022-09-22 16:13:31 +0200

    moving right & left

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,11 +31,15 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame._RIGHT:
+                if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
