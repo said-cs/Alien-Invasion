2daf162b830f82de08e92b26ce843d6a4605ca8b
said <charef.said@gmail.com>
2022-09-29 17:06:37 +0200

    Repopulating the fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -85,9 +85,17 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
-        # Check for any bullets that have hit aliens
-        # If so, get rid of the bullet and the alien
+        self._check_bullet_alien_collisions()
+        
+    def _check_bullet_alien_collisions(self):
+        """Respod to bullet-alien collisions"""
+        # Remove any bullets and aliens that have collided
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet
+            self.bullets.empty()
+            self._create_fleet()
 
     def _create_fleet(self):
         """Create the fleet of aliens"""
