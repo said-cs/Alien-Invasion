7ff6b6b15ce229ad4b20d0e0c5a29b33dc2d465f
said <charef.said@gmail.com>
2022-10-06 14:31:17 +0200

    resetting the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -188,6 +188,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _check_aliens_bottom(self):
         """Check if any aliens have reach the bottom of the screen"""
@@ -200,8 +201,23 @@ class AlienInvasion:
 
     def _check_play_button(self, mouse_pos):
         """Start a new game when the player clicks play"""
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
+            # Reset the game statistics
+            self.stats.reset_stats()
             self.stats.game_active = True
+
+            # Get rid of any remaining aliens and bullets
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # Create a new fleet and center the ship
+            self._create_fleet()
+            self.ship.center_ship()
+
+            # Hide the mouse cursor
+            pygame.mouse.set_visible(False)
+
 if __name__ == '__main__':
     # Make a game instance, and run the game
     ai = AlienInvasion()
